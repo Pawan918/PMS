734f49ff3ace53487035746015c3e07075d60b0e
Pawan918 <pawanshah84489@gmail.com>
2023-10-11 17:19:13 +0530

    final changes

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -3,14 +3,19 @@
         <div class="flex justify-between items-center h-full w-full mx-10">
             <div class=""><img src="./../assets/logo.svg" class="w-[80px] h-[70px] text-slate-50"/></div>
             <div class="flex items-center gap-4 relative">
+                <div class="relative cursor-pointer">
+                    <font-awesome-icon :icon="['fas', 'cart-shopping']" class="text-2xl p-2"/>
+                    <div class="absolute top-[-4px] right-[-6px] p-1 bg-[red] rounded-[25%] text-xs text-[white]"><span>5</span></div>
+                </div>
                 <div v-if="isLoggedIn">{{ firstName }}</div>
                 <div v-else>Login</div>
                 <font-awesome-icon :icon="['fas', 'circle-user']" class="text-2xl cursor-pointer p-4" @click="()=>isLogout=!isLogout" v-show="isLoggedIn"/>
                 <div v-show="isLogout && isLoggedIn" class="absolute right-2 top-12 bg-[black] w-20 h-16 z-[99]">
-                    <div class="flex p-[2px] justify-center cursor-pointer bg-[red]  m-2" @click.prevent="logoutHandler">
+                    <div class="flex p-[2px] justify-center cursor-pointer bg-[#150707]  m-2" @click.prevent="logoutHandler">
                         Logout
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
